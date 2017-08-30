2087e56737a246fc7c3501db01354a6e438653c2
mrbmadrid <mrbmadrid@gmail.com>
2017-08-29 17:10:43 -1000

    Add template for java stuff
    This is just the barebones template. It compiles with javac. The ant script doesn't work yet.

---
+++ xcl/src/com/xlio/XlReader.java
@@ -0,0 +1,10 @@
+import org.apache.poi.*;
+
+
+class Main
+{
+    public static void main(String[] argv) {
+        System.out.println("Get to work!!!");
+        System.out.println("Nothing is implemented yet.");
+    } 
+}
