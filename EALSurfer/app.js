2d2b26bee1ec7547974ccd83139fcf2dad9e12a9
Brian <brianutilityemail@gmail.com>
2017-09-02 04:55:33 +0000

    Created surfer.css adding more ejs functionality.

--- EALSurfer/app.js
+++ EALSurfer/app.js
@@ -1,8 +1,14 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.get("/", function(req, res){
     res.send("<h1>HACC2017 0xFEEDFACE landing page.");
+});
+
+app.get("/Form", function(req, res){
+    res.render("form")
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
