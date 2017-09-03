982ddec33202f84c40dd7e0722bbb4c4d62e1bf9
Noel Niles <noelniles@gmail.com>
2017-09-02 15:23:25 -1000

    Add functions to populate form

--- xcelreader/src/xlreader/XlReader.java
+++ xcelreader/src/xlreader/XlReader.java
@@ -10,6 +10,7 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+
 /**
  * This class represents a set of excel documents.
  *
@@ -20,7 +21,6 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  *
  * An XlReader is constructed with a filename.
  */
-
 public class XlReader {
     private final String filename;
     private final Workbook workbook;
@@ -112,10 +112,13 @@ public class XlReader {
     /**
      * Evaluate a formula in a cell.
      *
-     * @param sheetnumber
-     * @param cellreference
+     * @param   sheetnumber
+     * @param   cellreference
      */
     public void evaluateFormula(final int sheetnumber, final String cellreference) {
+        if (sheetnumber > this.nsheets) {
+            System.exit(1);
+        }
         FormulaEvaluator evaluator = this.workbook.getCreationHelper().createFormulaEvaluator();
         CellReference cellref = new CellReference(cellreference);
         Sheet sheet = this.workbook.getSheetAt(sheetnumber);
@@ -130,6 +133,95 @@ public class XlReader {
      */
     public int getNsheets() {
         return this.nsheets;
+    }
+
+
+    /**
+     * Get the value from a cell.
+     *
+     * Returns a map with {cell reference : value}.
+     *
+     * NOTE: Not all cells will have a value. If the cell is blank or doesn't exist
+     *       then the resulting map will have a null value. It is the caller's
+     *       responsibility to check for null.
+     * @param   sheetnumber     an integer
+     * @param   cellreferences   a string like "A3"
+     * @return  map             a hash map like {cellreference : value | null}
+     */
+    public HashMap<String, Object> getCellValue(final int sheetnumber, final String[] cellreferences) {
+
+        HashMap<String, Object> map = new HashMap<>();
+
+        for (String cr : cellreferences) {
+            Sheet sheet = this.workbook.getSheetAt(sheetnumber);
+            CellReference cellref = new CellReference(cr);
+            Row row = sheet.getRow(cellref.getRow());
+            Cell cell = row.getCell(cellref.getCol());
+
+            String addr = cell.getAddress().toString();
+
+            switch (cell.getCellTypeEnum()) {
+                case FORMULA:
+                    map.put(addr, cell.getCellFormula());
+                    break;
+                case BLANK:
+                    map.put(addr, cell.getRichStringCellValue());
+                    break;
+                case STRING:
+                    map.put(addr, cell.getStringCellValue());
+                    break;
+                case BOOLEAN:
+                    map.put(addr, cell.getBooleanCellValue());
+                    break;
+                case NUMERIC:
+                    map.put(addr, cell.getNumericCellValue());
+                    break;
+                case ERROR:
+                    map.put(addr, cell.getErrorCellValue());
+                    break;
+                case _NONE:
+                    map.put(addr, cell.getRichStringCellValue());
+                    break;
+            }
+        }
+        return map;
+    }
+
+    public Boolean populate(final int sheetnumber, HashMap<String, Object> map) {
+
+        Boolean success = false;
+
+        Sheet sheet = this.workbook.getSheetAt(sheetnumber);
+
+        for (HashMap.Entry<String, Object> item : map.entrySet()) {
+            CellReference ref = new CellReference(item.getKey());
+            Row row = sheet.getRow(ref.getRow());
+            Cell cell = row.getCell(ref.getCol());
+
+            switch (cell.getCellTypeEnum()) {
+                case FORMULA:
+                    break;
+                case BLANK:
+                    break;
+                case STRING:
+                    cell.setCellValue((String) item.getValue());
+                    success = true;
+                    break;
+                case BOOLEAN:
+                    cell.setCellValue((Boolean) item.getValue());
+                    success = true;
+                    break;
+                case NUMERIC:
+                    cell.setCellValue((Double) item.getValue());
+                    success = true;
+                    break;
+                case ERROR:
+                    break;
+                case _NONE:
+                    break;
+            }
+        }
+        return success;
     }
 
     public static void main(String[] args) throws IOException {
@@ -148,7 +240,11 @@ public class XlReader {
 
         System.out.format("nsheets: %d\n", xlreader.getNsheets());
 
-        xlreader.findAllFormulae();
+        //xlreader.findAllFormulae();
         xlreader.evaluateFormula(0, "A3");
+
+        String[] cells = {"D5", "D7", "D10", "D14", "C16", "D22", "D24", "D26"};
+        HashMap vals = xlreader.getCellValue(2, cells);
+        vals.forEach((k, v) -> System.out.format("Cell: %s   Value: %s\n", k, v));
     }
 }
